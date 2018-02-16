877c8af89d8d513b006fa53ddbebb4220334a86d
chakkapun <chakkapun@x10studio.com>
2018-02-16 19:04:06 +0700

    init

---
+++ Assets/Hi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(1, 1, 30, 20), "hello");
+    }
+}
